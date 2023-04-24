8e397a507a422611e2aa24f52e88878efcd9144e
Steve Lewis <stevelewis_99@hotmail.com>
2023-04-24 13:52:15 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,47 @@
+# Getting Started with Sisense EmbedSDK React
+
+## Using the EmbedSDK component in your application
+
+Install using npm
+```
+npm install --save sisensers/sisense-embedsdk-react
+```
+\
+Import the module into your react application
+```
+import SisenseDashboardEmbed from 'sisense-embedsdk-react'
+```
+\
+Minimum required is to provide a sisenseUrl and dashboardId in the props of the `SisenseDashboardEmbed` component:
+
+```
+<SisenseDashboardEmbed
+  sisenseUrl={sisenseUrl}
+  dashboardId={dashboardId}
+/>
+```
+\
+Other features can be contolled via the component props:
+
+```
+<SisenseDashboardEmbed
+  sisenseUrl={sisenseUrl}
+  urlParams={urlParams}
+  dashboardId={dashboardId}
+  showLeftPane={false}
+  showRightPane={true}
+  showToolbar={false}
+  themeId={sisenseThemeId}
+  volatile={true}
+  editMode={false}
+  unmountShouldDestroySisenseFrame={false}
+  unmountShouldUnloadEmbedSdk={false}
+  onDashboardLoaded={(event: any, args: any) => handleDashboardLoaded(event, args)} //call your own function when the dashboard has loaded
+  onDashboardFirstLoaded={(loadedDashboard: any) => setCurrentDashboardObject(loadedDashboard)}
+/>
+```
+
+# Quickstart example
+For a quickstart example, check out https://github.com/sisensers/sisense-embedsdk-react-sampleapp
+
+![react-embedSDK](https://user-images.githubusercontent.com/9842660/232162477-1871de35-25dd-4936-8e31-949180004439.gif)
---
+++ rollup.config.js
@@ -0,0 +1,60 @@
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+import dts from "rollup-plugin-dts";
+
+//import { terser } from "rollup-plugin-terser";
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import { createPathTransform } from 'rollup-sourcemap-path-transform'
+
+export default [
+  {
+    external: [
+      'react',
+      // fileURLToPath(
+      //   new URL(
+      //     'src/some-local-file-that-should-not-be-bundled.js',
+      //     import.meta.url
+      //   )
+      // ),
+      /node_modules/
+    ],
+    input: "./src/index.tsx",
+    output: [
+      {
+        file: "dist/cjs/index.js",
+        format: "cjs",
+        sourcemap: false,
+        // sourcemapPathTransform: createPathTransform({
+        //   prefixes: {
+        //     '*src/components/': '/sisense-embedsdk-react/'
+        //   }
+        // })
+      },
+      {
+        file: "dist/esm/index.js",
+        format: "esm",
+        sourcemap: false,
+        // sourcemapPathTransform: createPathTransform({
+        //   prefixes: {
+        //     '*src/components/': '/sisense-embedsdk-react/'
+        //   }
+        // })
+      },
+    ],
+    plugins: [
+      peerDepsExternal(),
+      resolve(),
+      commonjs(),
+      typescript({ tsconfig: "./tsconfig.json" }),
+      //terser()
+    ],
+  },
+  {
+    input: "dist/esm/index.d.ts",
+    output: [{ file: "dist/index.d.ts", format: "esm" }],
+    plugins: [dts()],
+
+    external: [/\.css$/], // telling rollup anything that is .css aren't part of type exports 
+  },
+]
